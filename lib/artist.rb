d97e280bbc222ca5be976e1258c9b64a3b9b267d
Billy Joseph <ingbillyjoseph@gmail.com>
2019-05-06 13:19:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,32 +1,35 @@
-require 'pry'
-
 class Artist
-  extend Memorable::ClassMethods
-  extend Findable::ClassMethods
-
-  include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
-
-  attr_accessor :name
-  attr_reader :songs
-
-  @@artists = []
-
-  def initialize
-    super
+  @@all = []
+  attr_accessor :name, :songs, :artist
+  extend Concerns::Findable
+  def initialize(name)
+    @name = name
     @songs = []
   end
 
   def self.all
-    @@artists
+    @@all
+  end
+
+  def self.destroy_all
+    all.clear
+  end
+
+  def save
+    Artist.all << self
+  end
+
+  def self.create(name)
+    new(name).tap {|artist| artist.save}
   end
 
   def add_song(song)
-    @songs << song
-    song.artist = self
+    @songs << song if !songs.include?(song)
+    song.artist = self if song.artist == ""
   end
 
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
+  def genres
+    songs.collect {|song|  song.genre}.uniq
   end
+
 end
